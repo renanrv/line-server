2c32b46e0ca18404a6084af49bb43b40c669bc29
Renan Vasconcelos <renan@unbabel.com>
2025-04-21 23:46:10 +0100

    Refactor file-generator tool to support as argument the size in GB for the output file

--- internal-tools/file-generator/main.go
+++ internal-tools/file-generator/main.go
@@ -9,24 +9,30 @@ import (
 	zlog "github.com/rs/zerolog/log"
 )
 
-// FileGenerator generates a file with a specified number of lines to be used for performance tests
+const (
+	BytesInGB = 1024 * 1024 * 1024
+)
+
+// main generates a file of a specified size in GB with lines numbered from 0 to n.
 func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	zeroLog := zlog.With().Caller().Str("component", "file-generator").Logger()
 
 	if len(os.Args) < 3 {
-		zeroLog.Fatal().Msg("Usage: go run internal-tools/file-generator/main.go <number_of_lines> <output_file_path>")
+		zeroLog.Fatal().Msg("Usage: go run internal-tools/file-generator/main.go <file_size_gb> <output_file_path>")
 	}
 
-	n, err := strconv.Atoi(os.Args[1])
-	if err != nil || n < 0 {
-		zeroLog.Fatal().Str("number_of_lines", os.Args[1]).Msg("Invalid number of lines")
+	sizeGB, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil || sizeGB <= 0 {
+		zeroLog.Fatal().Str("file_size_gb", os.Args[1]).Msg("Invalid file size in GB")
 	}
+	targetSize := int64(sizeGB * BytesInGB)
 
 	filePath := os.Args[2]
 	if filePath == "" {
-		zeroLog.Fatal().Str("output_file_path", os.Args[2]).Msg("Invalid output file path")
+		zeroLog.Fatal().Str("output_file_path", filePath).Msg("Invalid output file path")
 	}
+
 	file, err := os.Create(filePath)
 	if err != nil {
 		zeroLog.Fatal().Err(err).Msg("Could not create output file")
@@ -37,12 +43,19 @@ func main() {
 		}
 	}()
 
-	for i := 0; i < n; i++ {
-		line := fmt.Sprintf("Line %d\n", i)
-		_, err := file.WriteString(line)
+	var totalBytesWritten int64
+	var lineCount int
+	for totalBytesWritten < targetSize {
+		line := fmt.Sprintf("Line %d\n", lineCount)
+		n, err := file.WriteString(line)
 		if err != nil {
 			zeroLog.Fatal().Err(err).Msg("Could not write to output file")
 		}
+		totalBytesWritten += int64(n)
+		lineCount++
 	}
-	zeroLog.Info().Msg(fmt.Sprintf("Successfully wrote %d lines to %s", n, filePath))
+
+	zeroLog.Info().Msg(
+		fmt.Sprintf("Successfully wrote approximately %.2f GB (%d lines) to %s",
+			float64(totalBytesWritten)/BytesInGB, lineCount, filePath))
 }
